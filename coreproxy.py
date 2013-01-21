ffd2071c6aa455627d1b60f716e0ae576c6d6014
mark3139 <yibomark@gmail.com>
2013-01-21 22:41:25 +0800

    add crawl ip and validate

--- coreproxy.py
+++ coreproxy.py
@@ -3,6 +3,9 @@
 
 import urllib2
 import re
+import json
+import string
+import time
 
 from lxml import etree
 
@@ -24,8 +27,56 @@ class ProxyCrawl():
         content = self.tree.find('/body/table[@width="100%"]//pre').text
         proxys = re.findall(r'\d+\.\d+\.\d+\.\d+:\d+', content)
         proxys = [proxy.split(':') for proxy in proxys]
-        self.http_proxys = {ip: port for ip, port in proxys}
+        http_proxys = {ip: port for ip, port in proxys}
+        return http_proxys
+
+    def crawl_proxymore(self):
+        '''Crawl proxymore find proxy'''
+        url = 'http://www.proxymore.net/proxy_area-CN.html'
+        #web = self.opener.open(url)
+        #self.tree = etree.parse(web, self.parser)
+        web = file('proxy_area-CN.html')
+        self.tree = etree.parse('proxy_area-CN.html', self.parser)
+        port = re.search(r'\(port,\s({.*})', web.read())
+        if port:
+            port = re.sub(r'(\w)', "\"\g<1>\"", port.group(1))
+            port_map = json.loads(port)
+        else:
+            raise Exception("No port map")
+        trs = self.tree.findall(".//tr[@class='x-tr']")
+        http_proxys = {}
+        mt = string.maketrans(str(port_map.keys()), str(port_map.values()))
+        for proxy in trs:
+            ip = proxy.find("./td[@class='pserver']/span").text
+            port = proxy.find("./td[@class='pport']/span").text
+            port = port.translate(mt)
+            http_proxys[ip] = port
+
+        return http_proxys
+
+    def get_proxy(self):
+        self.http_proxys = self.crawl_ipcn()
+        self.http_proxys = dict(self.http_proxys, **self.crawl_proxymore())
+
+
+class Proxy():
+    def __init__(self, proxys):
+        self.test_url = 'http://www.baidu.com'
+
+    def validate(self, proxy):
+        proxy_handler = urllib2.ProxyHandler({'http': proxy})
+        opener = urllib2.build_opener(proxy_handler)
+        befor = time.time()
+        try:
+            o = opener.open(self.test_url)
+            if o:
+                after = time.time()
+                return (1, int((after - befor) * 1000))
+            else:
+                return (0, 0)
+        except Exception:
+            return (0, 0)
 
 if __name__ == '__main__':
-    p = ProxyCrawl()
-    p.crawl_ipcn()
+    p = Proxy('')
+    print p.validate('119.254.90.18:8080')
